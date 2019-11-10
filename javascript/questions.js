d3f9183dd1e6597fb3c4bbfdefa8845a01def61d
Kogan <koganp42@gmail.com>
2019-11-10 11:24:46 -0600

    Finished the javascript file containing the question list.

--- javascript/questions.js
+++ javascript/questions.js
@@ -9,6 +9,46 @@ var questions = [
       choices: ["Endor", "Kashyyk", "Dagobah", "Aldorran"],
       answer: "Kashyyk"
     },
+    {
+      title: "What race/species was Boba Fett?",
+      choices: ["Human", "Kith", "Mandalorian", "Alderrani"],
+      answer: "Mandalorian"
+    },
+    {
+      title: "How many engines are on an X-Wing Star Fighter?",
+      choices: ["Eight", "Two", "Five", "Four"],
+      answer: "Four"
+    },
+    {
+      title: "Before he left the jedi council, who was Count Dooku's apprentice?",
+      choices: ["Qui-Gon Jinn", "Obi-Wan Kenobi", "Wedge Antilles", "Rael Averross"],
+      answer: "Qui-Gon Jinn"
+    },
+    {
+      title: "What type of farm did Luke grow up on?",
+      choices: ["A Bantha Farm", "A Wompa Farm", "A Moisture Farm", "A Dew Farm"],
+      answer: "A Moisture Farm"
+    },
+    {
+      title: "Many -what- died while stealing the plans for the Death Star?",
+      choices: ["Rhodians", "Ewoks", "Bith", "Bothans"],
+      answer: "Bothans"
+    },
+    {
+      title: "Which famous character was supposed to appear in the original release of A New Hope but wasn't actually included as intended until 20 years later in a re-release of the film?",
+      choices: ["Boba Fett", "Leia", "Greedo", "Jabba The Hutt"],
+      answer: "Jabba The Hutt"
+    },
+    {
+      title: "What was the name of Darth Vader's personal Star Destroyer?",
+      choices: ["Executor", "Devastator", "Super Star Destroyer", "Leviathan"],
+      answer: "Devastator"
+    },
+    {
+      title: "Who shot first in the cantina scene where Han Solo is confronted by Greedo?",
+      choices: ["Greedo", "Han Solo", "A member of the cantina band", "Dr. Cornelius Evazan"],
+      answer: "Bothans"
+    },
 
     ///etc.
   ];
