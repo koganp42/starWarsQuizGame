a4f9d9c4bf24442ae74fe5405d631a8a3b147839
Kogan <koganp42@gmail.com>
2019-11-10 11:58:05 -0600

    Made the question choices into an object with its own key -value pairs
    I'm assuming doing this will make it slightly easier later on so I won't have to grab index values for all of the arrays.

--- javascript/questions.js
+++ javascript/questions.js
@@ -1,55 +1,103 @@
 var questions = [
     {
       title: "What does AT-AT stand for?",
-      choices: ["All Terrain-Attack Transport", "All Terrain-Armored Transport", "Armored Troop-Attack Transporter", "Always Take-A Towel"],
-      answer: "All Terrain-Armored Transport"
+      choices: {
+        A = "All Terrain-Attack Transport", 
+        B = "All Terrain-Armored Transport", 
+        C = "Armored Troop-Attack Transporter", 
+        D= "Always Take-A Towel",
+      },
+      answer: "B: All Terrain-Armored Transport"
     },
     {
       title: "What planet is home to Chewbacca and the Wookies?",
-      choices: ["Endor", "Kashyyk", "Dagobah", "Aldorran"],
-      answer: "Kashyyk"
+      choices: {
+        A = "Endor", 
+        B = "Kashyyk", 
+        C = "Dagobah", 
+        D = "Aldorran"
+    },
+      answer: "B: Kashyyk"
     },
     {
       title: "What race/species was Boba Fett?",
-      choices: ["Human", "Kith", "Mandalorian", "Alderrani"],
-      answer: "Mandalorian"
+      choices: {
+      A = "Human", 
+      B = "Kith", 
+      C = "Mandalorian", 
+      D = "Alderrani"
+      },
+      answer: "C: Mandalorian"
     },
     {
       title: "How many engines are on an X-Wing Star Fighter?",
-      choices: ["Eight", "Two", "Five", "Four"],
-      answer: "Four"
+      choices: {
+      A = "Eight", 
+      B = "Two", 
+      C = "Five", 
+      D = "Four"
+      },
+      answer: "D: Four"
     },
     {
       title: "Before he left the jedi council, who was Count Dooku's apprentice?",
-      choices: ["Qui-Gon Jinn", "Obi-Wan Kenobi", "Wedge Antilles", "Rael Averross"],
-      answer: "Qui-Gon Jinn"
+      choices: {
+      A = "Qui-Gon Jinn", 
+      B = "Obi-Wan Kenobi", 
+      C = "Wedge Antilles", 
+      D = "Rael Averross"
+      },
+      answer: "A: Qui-Gon Jinn"
     },
     {
       title: "What type of farm did Luke grow up on?",
-      choices: ["A Bantha Farm", "A Wompa Farm", "A Moisture Farm", "A Dew Farm"],
-      answer: "A Moisture Farm"
+      choices: {
+      A = "A Bantha Farm", 
+      B = "A Wompa Farm", 
+      C = "A Moisture Farm", 
+      D = "A Dew Farm"
+      },
+      answer: "C: A Moisture Farm"
     },
     {
       title: "Many -what- died while stealing the plans for the Death Star?",
-      choices: ["Rhodians", "Ewoks", "Bith", "Bothans"],
-      answer: "Bothans"
+      choices: {
+      A = "Rhodians", 
+      B = "Ewoks", 
+      C = "Bith", 
+      D = "Bothans"
+      },
+      answer: "D: Bothans"
     },
     {
       title: "Which famous character was supposed to appear in the original release of A New Hope but wasn't actually included as intended until 20 years later in a re-release of the film?",
-      choices: ["Boba Fett", "Leia", "Greedo", "Jabba The Hutt"],
-      answer: "Jabba The Hutt"
+      choices: {
+      A = "Boba Fett", 
+      B = "Leia", 
+      C = "Greedo", 
+      D = "Jabba The Hutt"
+      },
+      answer: "D: Jabba The Hutt"
     },
     {
       title: "What was the name of Darth Vader's personal Star Destroyer?",
-      choices: ["Executor", "Devastator", "Super Star Destroyer", "Leviathan"],
-      answer: "Devastator"
+      choices: {
+        A = "Executor", 
+        B = "Devastator", 
+        C = "Super Star Destroyer", 
+        D = "Leviathan"
+      },
+      answer: "B: Devastator"
     },
     {
-      title: "Who shot first in the cantina scene where Han Solo is confronted by Greedo?",
-      choices: ["Greedo", "Han Solo", "A member of the cantina band", "Dr. Cornelius Evazan"],
-      answer: "Bothans"
+      title: "Who originally shot first in the cantina scene where Han Solo is confronted by Greedo?",
+      choices: {
+        A = "Han Solo", 
+        B = "Greedo", 
+        C = "A member of the cantina band", 
+        D = "Dr. Cornelius Evazan"
+      },
+      answer: "A: Han Solo"
     },
-
-    ///etc.
   ];
   
