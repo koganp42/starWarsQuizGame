1892ec36b7e5c7d7e4994b0ceb21ddb2f401d63b
Kogan <koganp42@gmail.com>
2019-11-10 12:11:33 -0600

    Realized I was using the "=" operator for my answers, switched to ":"

--- javascript/questions.js
+++ javascript/questions.js
@@ -1,101 +1,101 @@
-var questions = [
+const questions = [
     {
       title: "What does AT-AT stand for?",
       choices: {
-        A = "All Terrain-Attack Transport", 
-        B = "All Terrain-Armored Transport", 
-        C = "Armored Troop-Attack Transporter", 
-        D= "Always Take-A Towel",
+        A: "All Terrain-Attack Transport", 
+        B: "All Terrain-Armored Transport", 
+        C: "Armored Troop-Attack Transporter", 
+        D: "Always Take-A Towel",
       },
       answer: "B: All Terrain-Armored Transport"
     },
     {
       title: "What planet is home to Chewbacca and the Wookies?",
       choices: {
-        A = "Endor", 
-        B = "Kashyyk", 
-        C = "Dagobah", 
-        D = "Aldorran"
+        A: "Endor", 
+        B: "Kashyyk", 
+        C: "Dagobah", 
+        D: "Aldorran"
     },
       answer: "B: Kashyyk"
     },
     {
       title: "What race/species was Boba Fett?",
       choices: {
-      A = "Human", 
-      B = "Kith", 
-      C = "Mandalorian", 
-      D = "Alderrani"
+      A: "Human", 
+      B: "Kith", 
+      C: "Mandalorian", 
+      D: "Alderrani"
       },
       answer: "C: Mandalorian"
     },
     {
       title: "How many engines are on an X-Wing Star Fighter?",
       choices: {
-      A = "Eight", 
-      B = "Two", 
-      C = "Five", 
-      D = "Four"
+      A: "Eight", 
+      B: "Two", 
+      C: "Five", 
+      D: "Four"
       },
       answer: "D: Four"
     },
     {
       title: "Before he left the jedi council, who was Count Dooku's apprentice?",
       choices: {
-      A = "Qui-Gon Jinn", 
-      B = "Obi-Wan Kenobi", 
-      C = "Wedge Antilles", 
-      D = "Rael Averross"
+      A: "Qui-Gon Jinn", 
+      B: "Obi-Wan Kenobi", 
+      C: "Wedge Antilles", 
+      D: "Rael Averross"
       },
       answer: "A: Qui-Gon Jinn"
     },
     {
       title: "What type of farm did Luke grow up on?",
       choices: {
-      A = "A Bantha Farm", 
-      B = "A Wompa Farm", 
-      C = "A Moisture Farm", 
-      D = "A Dew Farm"
+      A:"A Bantha Farm", 
+      B: "A Wompa Farm", 
+      C: "A Moisture Farm", 
+      D: "A Dew Farm"
       },
       answer: "C: A Moisture Farm"
     },
     {
       title: "Many -what- died while stealing the plans for the Death Star?",
       choices: {
-      A = "Rhodians", 
-      B = "Ewoks", 
-      C = "Bith", 
-      D = "Bothans"
+      A: "Rhodians", 
+      B: "Ewoks", 
+      C: "Bith", 
+      D: "Bothans"
       },
       answer: "D: Bothans"
     },
     {
       title: "Which famous character was supposed to appear in the original release of A New Hope but wasn't actually included as intended until 20 years later in a re-release of the film?",
       choices: {
-      A = "Boba Fett", 
-      B = "Leia", 
-      C = "Greedo", 
-      D = "Jabba The Hutt"
+      A: "Boba Fett", 
+      B: "Leia", 
+      C: "Greedo", 
+      D: "Jabba The Hutt"
       },
       answer: "D: Jabba The Hutt"
     },
     {
       title: "What was the name of Darth Vader's personal Star Destroyer?",
       choices: {
-        A = "Executor", 
-        B = "Devastator", 
-        C = "Super Star Destroyer", 
-        D = "Leviathan"
+        A: "Executor", 
+        B: "Devastator", 
+        C: "Super Star Destroyer", 
+        D: "Leviathan"
       },
       answer: "B: Devastator"
     },
     {
       title: "Who originally shot first in the cantina scene where Han Solo is confronted by Greedo?",
       choices: {
-        A = "Han Solo", 
-        B = "Greedo", 
-        C = "A member of the cantina band", 
-        D = "Dr. Cornelius Evazan"
+        A: "Han Solo", 
+        B: "Greedo", 
+        C: "A member of the cantina band", 
+        D: "Dr. Cornelius Evazan"
       },
       answer: "A: Han Solo"
     },
