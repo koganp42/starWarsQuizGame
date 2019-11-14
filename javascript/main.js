96c3e76a42b7af801d642555fd2edec15123736c
Kogan <koganp42@gmail.com>
2019-11-14 17:39:48 -0600

    Quiz will now cycle through questions correctly. Needs an exit function.

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,24 @@
 let quizEl = document.querySelector("#quiz");
 let quizButton = document.querySelector("#quizStart");
 let timerEl = document.querySelector("#timer");
+let quizInfoEl = document.querySelector("#startingInfo");
 let quizResults = document.querySelector("quizResults");
-let questionsEl = document.querySelector("#questions");
+let titleEl = document.querySelector("#title")
 let choicesEl = document.querySelector("#choices");
-// let lastQuestionIndex = questions.length - 1;
-// let startingQuestionIndex = 0;
+let firstChoiceEl = document.querySelector("#choices1");
+let secondChoiceEl = document.querySelector("#choices2");
+let thirdChoiceEl = document.querySelector("#choices3");
+let fourthChoiceEl = document.querySelector("#choices4");
+let currentQuestionIndex = 0;
 let interval;
 let timeLeft = 150;
 
 //This function, triggered by a click event, will be responsible for housing the entire quiz and its html generation. Should just be the interval, giving the time allowed for the quiz, and invoke the quizLoop function. Anything else needed?
 function startQuiz(){
     interval = setInterval(countdown, 1000);
-    //quizLoop function I assume will go here.
     quizLoop();
+    quizDisplaySwitch();
+    showQuestion();
 }
 
 //A countdown function that's called when the Start Quiz button is clicked.
@@ -28,52 +33,52 @@ function countdown(){
     }
 }
 
-//Need to create a function that will be called by startQuiz, which will replace the contents of quizBody with the first question and set of answer choices. Choices will be clickable, and when any of the four are clicked it will replace the html with that of the next question and so on. Will need to also check the selection against the correct answer, and if it isn't that answer, have it deduct 10 seconds from the timer. 
-function quizLoop() {
-    //Assigning the key/value pairs to variables of their own with the intention of using the variables in the looping creation of html that contains each question title and its choices. Not sure if this is the right thing to do.
-    let quizTitle = questions[i].title;
-    let quizChoices = questions[i].choices;
-    let quizAnswer = questions[i].answer;
-    let lastQuestion = questions.length - 1;
-    let currentQuestion = 0;
-    
-    function generateQuestion(){
-        let q = questions[currentQuestion];
-        quizEl.innerHTML = 
-        `<div class="row" id="questions">${q.title}</div>
-        <div class="row choices">${q.choices[0]}</div>
-        <div class="row choices">${q.choices[1]}</div>
-        <div class="row choices">${q.choices[2]}</div>
-        <div class="row choices">${q.choices[3]}</div>`
-    }
-
-//I'm assuming I'll need a for loop to loop through the titles and choices, 
-    // for (i = 0; i < questions.length; i++){
-    //     quizEl.innerHTML = 
-    //     `<div class="row" id="questions">${quizTitle}</div>
-    //     <div class="row choices">A) ${quizChoices[0]}</div>
-    //     <div class="row choices">B) ${quizChoices[1]}</div>
-    //     <div class="row choices">C) ${quizChoices[2]}</div>
-    //     <div class="row choices">D) ${quizChoices[3]}</div>`;
-    // }
-
-    // quizChoices.addEventListener("click", checkAnswer);
-
-    // function checkAnswer(){
-    //     if (quizChoices !== quizAnswer){
-    //         timeLeft -= 10;
-
-    //     } 
-    // }
+function quizDisplaySwitch() {
+    quizInfoEl.style.display = "none";
+    quizEl.style.display = "block";
 }
 
 
+//Need to create a function that will be called by startQuiz, which will replace the contents of quizBody with the first question and set of answer choices. Choices will be clickable, and when any of the four are clicked it will replace the html with that of the next question and so on. Will need to also check the selection against the correct answer, and if it isn't that answer, have it deduct 10 seconds from the timer. 
+
+function showQuestion() {
+    titleEl.textContent = questions[currentQuestionIndex].title;
+    firstChoiceEl.textContent = questions[currentQuestionIndex].choices[0];
+    secondChoiceEl.textContent = questions[currentQuestionIndex].choices[1];
+    thirdChoiceEl.textContent = questions[currentQuestionIndex].choices[2];
+    fourthChoiceEl.textContent = questions[currentQuestionIndex].choices[3];
+}
+
+function checkAnswer(){
+    let dataValue = event.target.getAttribute("data-value");
+    
+    console.log(dataValue);
+    if (dataValue === questions[currentQuestionIndex].answer){
+        currentQuestionIndex++;
+        showQuestion();
+    }else{
+        timeLeft -= 15;
+        currentQuestionIndex++;
+        showQuestion();
+    }
+    
+}
 function results(){
     
 }
 
 quizButton.addEventListener("click", startQuiz);
 
+firstChoiceEl.addEventListener("click", checkAnswer);
+secondChoiceEl.addEventListener("click", checkAnswer);
+thirdChoiceEl.addEventListener("click", checkAnswer);
+fourthChoiceEl.addEventListener("click", checkAnswer);
+// quizEl.innerHTML = 
+// `<div class="row" id="questions">${q.title}</div>
+    // <div class="row choices">${q.choices[0]}</div>
+    // <div class="row choices">${q.choices[1]}</div>
+    // <div class="row choices">${q.choices[2]}</div>
+    // <div class="row choices">${q.choices[3]}</div>`
 
 //The code commented out below was based on an example I found online for generating quiz questions through js using my questions object. I'm not sure if it'll actually work, so I've commented it out and tried a different way above this line.
     // function quizLoop(){
@@ -97,4 +102,4 @@ quizButton.addEventListener("click", startQuiz);
     //         }
     //     );
     //     quizEl.innerHTML = output.join('');
-    // }
+    // 
--- javascript/questions.js
+++ javascript/questions.js
@@ -2,52 +2,52 @@ const questions = [
     {
       title: "What does AT-AT stand for?",
       choices: ["All Terrain-Attack Transport", "All Terrain-Armored Transport", "Armored Troop-Attack Transporter", "Always Take-A Towel"],
-      answer: "All Terrain-Armored Transport"
+      answer: "1"
     },
     {
       title: "What planet is home to Chewbacca and the Wookies?",
       choices: ["Endor", "Kashyyk", "Dagobah", "Aldorran"],
-      answer: "Kashyyk"
+      answer: "1"
     },
     {
       title: "What race/species was Boba Fett?",
       choices: ["Human", "Kith", "Mandalorian", "Alderrani"],
-      answer: "Mandalorian"
+      answer: "2"
     },
     {
       title: "How many engines are on an X-Wing Star Fighter?",
       choices: ["Eight", "Two", "Five", "Four"],
-      answer: "Four"
+      answer: "3"
     },
     {
       title: "Before he left the jedi council, who was Count Dooku's apprentice?",
       choices: ["Qui-Gon Jinn", "Obi-Wan Kenobi", "Wedge Antilles", "Rael Averross"],
-      answer: "Qui-Gon Jinn"
+      answer: "0"
     },
     {
       title: "What type of farm did Luke grow up on?",
       choices: ["A Bantha Farm", "A Wompa Farm", "A Moisture Farm", "A Dew Farm"],
-      answer: "A Moisture Farm"
+      answer: "2"
     },
     {
       title: "Many -what- died while stealing the plans for the Death Star?",
       choices: ["Rhodians", "Ewoks", "Bith", "Bothans"],
-      answer: "Bothans"
+      answer: "3"
     },
     {
       title: "Which famous character was supposed to appear in the original release of A New Hope but wasn't actually included as intended until 20 years later in a re-release of the film?",
       choices: ["Boba Fett", "Leia", "Greedo", "Jabba The Hutt"],
-      answer: "Jabba The Hutt"
+      answer: "3"
     },
     {
       title: "What was the name of Darth Vader's personal Star Destroyer?",
       choices: ["Executor", "Devastator", "Super Star Destroyer", "Leviathan"],
-      answer: "Devastator"
+      answer: "1"
     },
     {
       title: "Who originally shot first in the cantina scene where Han Solo is confronted by Greedo?",
       choices: ["Han Solo", "Greedo", "A member of the cantina band", "Dr. Cornelius Evazan"],
-      answer: "Han Solo"
+      answer: "0"
     },
   ];
   
